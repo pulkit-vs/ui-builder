4f61db36cc311afff365f0ce8d25d306153277f5
Mahima <46838111+mahima-vs@users.noreply.github.com>
2020-04-11 16:18:11 +0530

    Nested Components. (#3)
    * Code refractor for theme, adding defaultprops and proptypes for validation, Adding nested components module.
    
    * Improving logging.
    
    Co-authored-by: mahimagupta <mahima.gupta@kickrumtech.com>

--- examples/src/index.js
+++ examples/src/index.js
@@ -6,12 +6,12 @@
  * @flow strict-local
  */
 
-
 import ButtonComponent from "./components/button";
 import Input from "./components/input";
 import React from "react";
 import { View } from "react-native";
 import { get } from "lodash";
+import {Header} from "react-native-elements";
 
 export default class UiBuilder extends React.Component {
   constructor() {
@@ -19,13 +19,31 @@ export default class UiBuilder extends React.Component {
     this.selectComponent = this.selectComponent.bind(this);
   }
 
-  selectComponent(component, theme) {
+  selectComponent(component) {
     const type = get(component, "type", "");
     switch (type) {
       case "input":
-        return <Input componentData={component} />;
+        return <Input {...component.properties} />;
       case "button":
-        return <ButtonComponent componentData={component} />;
+        return <ButtonComponent {...component.properties} />;
+      case "view":
+        return (
+          <View>
+            {/* TODO: will add header in a seperate class later */}
+            <Header 
+              backgroundColor="white"
+              containerStyle = {{borderBottomColor : 'grey', borderBottomWidth: 1}}
+              fontSize = {30}
+              width = {"10%"}
+              placement="left"
+              leftComponent={{ text: 'Create an account to checkout', style: { color: 'black', fontSize: 20}}}
+              // rightComponent={{ icon: 'cross', color: 'black' }}
+            />
+            {component.childrens.map(componentData => {
+              return this.selectComponent(componentData);
+            })}
+          </View>
+        )
     }
   }
 
