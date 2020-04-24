1952848a99187a4fdb7a172edbeeaf9bf21342e3
Mahima <46838111+mahima-vs@users.noreply.github.com>
2020-04-24 21:56:40 +0530

    Login page (#10) (#16)
    * commented json is for new screen
    
    * solved header theme and proptypes issue
    
    * remove commented code of theme
    
    * deleted the theme from src and add in examples/src/theme
    
    * applied generic theme for also header sub components
    
    * applied theme on  view of index.js and remove nested loop in header
    
    Co-authored-by: utkarsh3326 <41411657+utkarsh3326@users.noreply.github.com>

--- examples/App.js
+++ examples/App.js
@@ -10,25 +10,46 @@ import UiBuilder from 'react-native-ui-builder';
 import React from 'react';
 
 const source = {
-  // theme: 'darkTheme',
-  data: [{
-    type: 'button',
-    properties: {
+  theme: 'lightTheme',
+  data: [
+    {
+      type: 'input',
+      properties: {
+        label: 'Mobile Number (10 digits)',
+        placeholderTextColor: 'grey',
+        style: {
+          backgroundColor: 'white',
+          borderColor: 'grey',
+          borderWidth: 2,
+          width: '90%',
+          left: 20,
+          marginTop: 40,
+        },
+      },
+    },
+    {
+      type: 'button',
+      properties: {
         title: 'FACEBOOK',
-        showCircle: true,
-        buttonStyle : {},
-        // icon: {
-        //   name: 'facebook',
-        //   color: 'blue',
-        //   size: 20
-        // }
-    }
-}
-]
-}
+        titleStyle: {color: 'blue', fontSize: 20},
+        buttonStyle: {
+          backgroundColor: 'white',
+          width: '90%',
+          left: 20,
+          borderWidth: 3,
+          marginTop: 20,
+        },
+        icon: {
+          name: 'facebook',
+          color: 'blue',
+          size: 20,
+        },
+      },
+    },
+  ],
+};
 
 export default class App extends React.Component {
-
   render() {
     return (
       <>
--- index.js
+++ index.js
@@ -7,16 +7,17 @@
  */
 
 import React from "react";
-import { View } from 'react-native';
+import { View } from "react-native";
 import { get } from "lodash";
 
 import ButtonComponent from "./src/components/button";
-import HeaderComponent from './src/components/header';
-import IconComponent from './src/components/icon';
-import ImageComponent from './src/components/image';
+import HeaderComponent from "./src/components/header";
+import IconComponent from "./src/components/icon";
+import ImageComponent from "./src/components/image";
 import Input from "./src/components/input";
 import ModalComponent from "./src/components/modal";
 import TextComponent from "./src/components/text";
+import { applyTheme } from "./src/utility/utils";
 
 // Global variable to get theme type in other files.
 export let theme;
@@ -27,31 +28,35 @@ export default class UiBuilder extends React.Component {
     this.selectComponent = this.selectComponent.bind(this);
   }
 
-  selectComponent(component) {
-    const type = get(component, 'type', '');
+  selectComponent(component, index) {
+    const type = get(component, "type", "");
     switch (type) {
-      case 'input':
-        return <Input {...component.properties} key={1} />;
-      case 'icon':
-        return <IconComponent {...component.properties} key={2} />;
-      case 'button':
-        return <ButtonComponent {...component.properties} key={3} />;
-      case 'header':
-        return <HeaderComponent {...component.properties} key={4} />;
+      case "input":
+        return <Input {...component.properties} key={index} />;
+      case "icon":
+        return <IconComponent {...component.properties} key={index} />;
+      case "button":
+        return <ButtonComponent {...component.properties} key={index} />;
+      case "header":
+        return <HeaderComponent {...component.properties} key={index} />;
       case "text":
-        return <TextComponent {...component.properties} key={5} />;
+        return <TextComponent {...component.properties} key={index} />;
       case "modal":
-        return <ModalComponent {...component} key={6} />
-      case 'view':
+        return <ModalComponent {...component} key={index} />;
+      case "view": {
+        if (theme) {
+          component.style = applyTheme(component.style, theme);
+        }
         return (
-          <View style={component.style} key={7} >
-            {component.childrens.map((componentData) => {
-              return this.selectComponent(componentData);
+          <View style={component.style} key={index}>
+            {component.childrens.map((componentData, i) => {
+              return this.selectComponent(componentData, i);
             })}
           </View>
         );
-      case 'image':
-        return <ImageComponent {...component.properties} key={8} />;
+      }
+      case "image":
+        return <ImageComponent {...component.properties} key={index} />;
     }
   }
 
@@ -60,8 +65,8 @@ export default class UiBuilder extends React.Component {
     theme = source.theme;
     return (
       <View>
-        {source.data.map((component) => {
-          return this.selectComponent(component);
+        {source.data.map((component, index) => {
+          return this.selectComponent(component, index);
         })}
       </View>
     );
