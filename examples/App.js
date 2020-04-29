018b885537b0caa34ec6c6921e1b249e25b5c589
mahimagupta <mahima.gupta@kickrumtech.com>
2020-04-29 17:58:07 +0530

    Voice recognition service.

--- examples/App.js
+++ examples/App.js
@@ -6,14 +6,12 @@
  * @flow strict-local
  */
 
-// import UiBuilder from 'react-native-ui-builder';
 import React from 'react';
 import { Text, View } from 'react-native';
 import { Button } from "react-native-elements";
 import Voice from '@react-native-community/voice';
 import Tts from 'react-native-tts';
-// import { Permissions } from "expo";
-import * as Permissions from 'expo-permissions';
+// import * as Permissions from 'expo-permissions';
 
 export default class App extends React.Component {
 
@@ -58,7 +56,6 @@ export default class App extends React.Component {
       results: e.value,
     });
 
-    this.setState({ textResponse: '' })
     await Object.keys(this.responses).map(key => {
       if (key === this.state.results[0]) {
         this.setState({ textResponse: this.responses[key] })
@@ -76,16 +73,12 @@ export default class App extends React.Component {
     // input.map((val) => Tts.speak(val));
   };
 
-  onStartButtonPress(e) {
-    console.log('onStartButtonPress:e->', e);
-    Voice.start('en-US');
-  }
-
   async _startRecognition(e) {
     this.setState({
       recognized: '',
       started: '',
       results: [],
+      textResponse: ''
     });
     try {
       await Voice.start('en-US');
@@ -94,28 +87,21 @@ export default class App extends React.Component {
     }
   }
 
-  onReleaseButton(e) {
-    console.log('onReleaseButton:e->', e);
-    Voice.stop();
-    console.log('result:', this.state.results)
-  }
+  // async componentDidMount() {
 
-  async componentDidMount() {
-
-    console.log('componentDidMount:', Permissions.AUDIO_RECORDING);
-    let { status, expires, permissions } = Permissions && await Permissions.askAsync(
-      Permissions.AUDIO_RECORDING
-    );
-    console.log('status:', status)
-    if (status !== "granted") {
-      console.log('Permissions');
-    } else {
-      console.log('no Permissions');
-    }
-  }
+  //   console.log('componentDidMount:', Permissions.AUDIO_RECORDING);
+  //   let { status, expires, permissions } = Permissions && await Permissions.askAsync(
+  //     Permissions.AUDIO_RECORDING
+  //   );
+  //   console.log('status:', status)
+  //   if (status !== "granted") {
+  //     console.log('Permissions');
+  //   } else {
+  //     console.log('no Permissions');
+  //   }
+  // }
 
   render() {
-    console.log('Result:', this.state.results);
     return (
       <View>
         <Text> Voice Recognition Service </Text>
