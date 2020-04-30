ce66ecced5d2f18c61494237bd6822c9f6167ad5
mahimagupta <mahima.gupta@kickrumtech.com>
2020-04-30 21:16:45 +0530

    Minor fixes of Json.

--- examples/App.js
+++ examples/App.js
@@ -12,6 +12,7 @@ import UiBuilder from './src/navigation/navigation';
 import data from "./src/data/jsonData";
 
 export default class App extends React.Component {
+  
   render() {
     return <UiBuilder source={data.navigationData} />;
   }
