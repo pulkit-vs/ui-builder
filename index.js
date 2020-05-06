ed0ef83c826430fbaf92b1d64e1e2fccd3fada6c
utkarsh3326 <41411657+utkarsh3326@users.noreply.github.com>
2020-05-06 08:08:46 -0700

    shifted code in outer src

--- index.js
+++ index.js
@@ -7,22 +7,24 @@
  */
 
 import React from "react";
-import { View } from "react-native";
 import { get } from "lodash";
+import { View, KeyboardAvoidingView, ScrollView } from "react-native";
 
+import { applyTheme } from "./src/utility/utils";
 import ButtonComponent from "./src/components/button";
+import { COMPONENTS } from "./src/utility/constant";
 import HeaderComponent from "./src/components/header";
 import IconComponent from "./src/components/icon";
 import ImageComponent from "./src/components/image";
-import Input from "./src/components/input";
+import TextInput from "./src/components/input";
 import ModalComponent from "./src/components/modal";
 import TextComponent from "./src/components/text";
-import { applyTheme } from "./src/utility/utils";
+import DividerComponent from "./src/components/divider";
 
 // Global variable to get theme type in other files.
 export let theme;
 
-export default class UiBuilder extends React.Component {
+export default class Components extends React.Component {
   constructor() {
     super();
     this.selectComponent = this.selectComponent.bind(this);
@@ -31,19 +33,44 @@ export default class UiBuilder extends React.Component {
   selectComponent(component, index) {
     const type = get(component, "type", "");
     switch (type) {
-      case "input":
-        return <Input {...component.properties} key={index} />;
-      case "icon":
+      case COMPONENTS.INPUT:
+        return <TextInput {...component.properties} key={index} />;
+      case COMPONENTS.ICON:
         return <IconComponent {...component.properties} key={index} />;
-      case "button":
-        return <ButtonComponent {...component.properties} key={index} />;
-      case "header":
-        return <HeaderComponent {...component.properties} key={index} />;
-      case "text":
-        return <TextComponent {...component.properties} key={index} />;
-      case "modal":
-        return <ModalComponent {...component} key={index} />;
-      case "view": {
+      case COMPONENTS.BUTTON:
+        return (
+          <ButtonComponent
+            {...component.properties}
+            createScreen={this.props.createScreen}
+            key={index}
+          />
+        );
+      case COMPONENTS.HEADER:
+        return (
+          <HeaderComponent
+            {...component.properties}
+            createScreen={this.props.createScreen}
+            key={index}
+          />
+        );
+      case COMPONENTS.TEXT:
+        return (
+          <TextComponent
+            {...component.properties}
+            createScreen={this.props.createScreen}
+            key={index}
+          />
+        );
+      case COMPONENTS.MODAL:
+        return (
+          <ModalComponent
+            {...component}
+            createScreen={this.props.createScreen}
+            key={index}
+          />
+        );
+
+      case COMPONENTS.VIEW: {
         if (theme) {
           component.style = applyTheme(component.style, theme);
         }
@@ -55,8 +82,10 @@ export default class UiBuilder extends React.Component {
           </View>
         );
       }
-      case "image":
+      case COMPONENTS.IMAGE:
         return <ImageComponent {...component.properties} key={index} />;
+      case COMPONENTS.DIVIDER:
+        return <DividerComponent {...component.properties} key={index} />;
     }
   }
 
@@ -64,11 +93,19 @@ export default class UiBuilder extends React.Component {
     const { source } = this.props;
     theme = source.theme;
     return (
-      <View>
-        {source.data.map((component, index) => {
-          return this.selectComponent(component, index);
-        })}
-      </View>
+      <KeyboardAvoidingView
+        enabled
+        behavior={"position"}
+        keyboardVerticalOffset={-200}
+      >
+        <ScrollView>
+          <View>
+            {source.data.map((component, index) => {
+              return this.selectComponent(component, index);
+            })}
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     );
   }
 }
--- src/components/button.js
+++ src/components/button.js
@@ -6,58 +6,81 @@
  * @flow strict-local
  */
 
-import Icon from 'react-native-vector-icons/FontAwesome';
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Button } from 'react-native-elements';
-import { View } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
+import PropTypes from "prop-types";
+import React from "react";
+import { Button } from "react-native-elements";
+import { View } from "react-native";
+import { NavigationContext } from "@react-navigation/native";
 
-import { applyTheme } from '../utility/utils';
-import { shapeStyles } from '../style/buttonStyle';
-import { theme } from '../../index';
+import { applyTheme } from "../utility/utils";
+import { shapeStyles } from "../style/buttonStyle";
+import { theme } from "../../index";
 
 export default class ButtonComponent extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      properties: {}
-    }
+      properties: {},
+    };
   }
+  static contextType = NavigationContext;
 
   async componentDidMount() {
-
-    await this.setState({ properties: this.props })
+    // Creating Screen for Button onPress
+    if (this.props.onPress) {
+      this.props.onPress.navigation
+        ? this.props.createScreen(this.props.onPress)
+        : null;
+    }
+    await this.setState({ properties: this.props });
 
     // Customizing button style
     const shapeStyle = this.props.showCircle
       ? { ...shapeStyles.circleShapeView }
       : this.props.showSquare
-        ? { ...shapeStyles.squareShapeView }
-        : this.props.showRectangle
-          ? { ...shapeStyles.rectangleShapeView }
-          : null;
+      ? { ...shapeStyles.squareShapeView }
+      : this.props.showRectangle
+      ? { ...shapeStyles.rectangleShapeView }
+      : null;
 
     if (shapeStyle) {
-      await this.setState({ properties: { ...this.state.properties, buttonStyle: { ...this.state.properties.buttonStyle, ...shapeStyle } } })
+      await this.setState({
+        properties: {
+          ...this.state.properties,
+          buttonStyle: { ...this.state.properties.buttonStyle, ...shapeStyle },
+        },
+      });
     }
 
     // Applying theme on button style
     if (theme) {
-      await this.setState({ properties: applyTheme(this.state.properties, theme) })
+      await this.setState({
+        properties: applyTheme(this.state.properties, theme),
+      });
 
       if (this.props.icon) {
-        await this.setState({ properties: { ...this.state.properties, icon: applyTheme(this.state.properties.icon, theme) } })
+        await this.setState({
+          properties: {
+            ...this.state.properties,
+            icon: applyTheme(this.state.properties.icon, theme),
+          },
+        });
       }
     }
   }
 
   render() {
+    const navigation = this.context;
     return (
       <View>
         <Button
           title={this.state.properties.title}
-          onPress={() => this.state.properties.onPress()}
+          onPress={() => {
+            this.props.onPress.navigation
+              ? navigation.navigate(this.props.onPress.screenName)
+              : this.props.onPress();
+          }}
           disabled={this.state.properties.disabled}
           buttonStyle={this.state.properties.buttonStyle}
           titleStyle={this.state.properties.titleStyle}
@@ -73,7 +96,7 @@ export default class ButtonComponent extends React.Component {
 // To run typechecking on the props for a component, for validating a props
 ButtonComponent.propTypes = {
   title: PropTypes.string,
-  onPress: PropTypes.func,
+  onPress: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),
   disabled: PropTypes.bool,
   buttonStyle: PropTypes.shape({
     backgroundColor: PropTypes.string,
@@ -88,18 +111,18 @@ ButtonComponent.propTypes = {
 
 // Defaultprops is to set the default props for the class.
 ButtonComponent.defaultProps = {
-  title: 'Button',
-  onPress: () => console.log('Please attach a method to this component'),
+  title: "Button",
+  onPress: () => console.log("Please attach a method to this component"),
   disabled: false,
   buttonStyle: {
-    backgroundColor: 'yellow',
-    borderColor: 'yellow',
+    backgroundColor: "yellow",
+    borderColor: "yellow",
   },
   titleStyle: {
-    color: 'blue',
-    fontFamily: 'arial',
+    // color: 'blue',
+    fontFamily: "arial",
     fontSize: 40,
   },
-  type: 'solid',
+  type: "solid",
   loading: false,
 };
