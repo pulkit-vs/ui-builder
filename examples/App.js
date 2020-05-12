493b17be9cc8ca92a078175d2cca5c34dfa7a1da
mahimagupta <mahima.gupta@kickrumtech.com>
2020-05-12 12:03:35 +0530

    Minor Fixes.

--- examples/App.js
+++ examples/App.js
@@ -1,17 +1,14 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * Sample React Native Ui Builder App
+ * https://github.com/pulkit-vs/ui-builder
  *
  * @format
  * @flow strict-local
  */
 
-// TODO: Will use package later.
-//TODO : backPress not working with debug Navigator Screen
-
-//import UiBuilder from 'react-native-ui-builder';
+//TODO : BackPress not working with debug Navigator Screen
+import UiBuilder from 'react-native-ui-builder';
 import React from 'react';
-import UiBuilder from './src/navigation/navigation';
 import data from './src/data/jsonData';
 
 export default class App extends React.Component {
