b0bb6582180f7c9c4f33d6b784c101e1dd323054
mahimagupta <mahima.gupta@kickrumtech.com>
2020-05-13 14:52:02 +0530

    Added carousel json.

--- examples/App.js
+++ examples/App.js
@@ -7,12 +7,13 @@
  */
 
 //TODO : BackPress not working with debug Navigator Screen
-import UiBuilder from 'react-native-ui-builder';
+// import UiBuilder from 'react-native-ui-builder';
+import UiBuilder from './src/navigation';
 import React from 'react';
 import data from './src/data/jsonData';
 
 export default class App extends React.Component {
   render() {
-    return <UiBuilder source={data.debugNavigator} />;
+    return <UiBuilder source={data.carouselSource} />;
   }
 }
